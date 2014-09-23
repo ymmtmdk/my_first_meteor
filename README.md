08b2d1be8edbd034d3bd3781f845dc8dbb8658aa
ymmtmdk <ymmtmdk@gmail.com>
2014-09-23 17:44:25 +0900

    Add jade.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 simple meteor app.
 
 * using coffeescript.
+* using jade.
 * using accounts.
 * removed insecure.
 * removed autopublish.
--- create.sh
+++ create.sh
@@ -1,9 +1,10 @@
 meteor create my_first_meteor
+rm my_first_meteor/my_first_meteor.html
 cp -r src/* my_first_meteor
 
 cd my_first_meteor
 rm my_first_meteor.js
 meteor remove insecure autopublish
-meteor add coffeescript accounts-password accounts-ui
+meteor add coffeescript accounts-password accounts-ui mquandalle:jade
 
 echo "run 'cd my_first_meteor && meteor '."
